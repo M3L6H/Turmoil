f89a69c9ab4c396e36cf013dbb0c3f663fd34404
M3L6H <sisoac@hotmail.com>
2020-06-19 01:00:25 -0700

    Made some awesome buttons

--- frontend/chaos.jsx
+++ frontend/chaos.jsx
@@ -5,5 +5,42 @@ import { Button } from './components/shoebuckle';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<Button>Hello World</Button>, root);
+    ReactDOM.render(<div className="test">
+        <div className="test-row">
+            <Button>Hello World</Button>
+            <Button primary>Hello World</Button>
+            <Button secondary>Hello World</Button>
+            <Button content="Shorthand" primary />
+        </div>
+        <div className="test-row">
+            <Button basic>Hello World</Button>
+            <Button basic red>Hello World</Button>
+            <Button basic orange>Hello World</Button>
+            <Button basic yellow>Hello World</Button>
+            <Button basic olive>Hello World</Button>
+            <Button basic green>Hello World</Button>
+            <Button basic teal>Hello World</Button>
+            <Button basic blue>Hello World</Button>
+            <Button basic violet>Hello World</Button>
+            <Button basic purple>Hello World</Button>
+            <Button basic pink>Hello World</Button>
+            <Button basic grey>Hello World</Button>
+            <Button basic black>Hello World</Button>
+        </div>
+        <div className="test-row inverted">
+            <Button basic inverted>Hello World</Button>
+            <Button basic inverted red>Hello World</Button>
+            <Button basic inverted orange>Hello World</Button>
+            <Button basic inverted yellow>Hello World</Button>
+            <Button basic inverted olive>Hello World</Button>
+            <Button basic inverted green>Hello World</Button>
+            <Button basic inverted teal>Hello World</Button>
+            <Button basic inverted blue>Hello World</Button>
+            <Button basic inverted violet>Hello World</Button>
+            <Button basic inverted purple>Hello World</Button>
+            <Button basic inverted pink>Hello World</Button>
+            <Button basic inverted grey>Hello World</Button>
+            <Button basic inverted black>Hello World</Button>
+        </div>
+    </div>, root);
 });
--- frontend/components/shoebuckle/button.jsx
+++ frontend/components/shoebuckle/button.jsx
@@ -1,11 +1,53 @@
 import React from 'react';
 
-export default ({ children }) => {
+export default (props) => {
+    const { 
+        children, 
+        content, 
+        primary, 
+        secondary, 
+        basic,
+        inverted,
+        red,
+        orange,
+        yellow,
+        olive,
+        green,
+        teal,
+        blue,
+        violet,
+        purple,
+        pink,
+        grey,
+        black,
+        onClick 
+    } = props;
+
+    const className = `shoebuckle btn 
+        ${ primary && "primary" } 
+        ${ secondary && "secondary" } 
+        ${ basic && "basic" }
+        ${ inverted && "inverted" }
+        ${ red && "red" }
+        ${ orange && "orange" }
+        ${ yellow && "yellow" }
+        ${ olive && "olive" }
+        ${ green && "green" }
+        ${ teal && "teal" }
+        ${ blue && "blue" }
+        ${ violet && "violet" }
+        ${ purple && "purple" }
+        ${ pink && "pink" }
+        ${ grey && "grey" }
+        ${ black && "black" }
+    `;
+    
     return (
         <button
-            className="shoebuckle btn"
+            className={ className }
+            onClick={ onClick }
         >
-            { children }
+            { children || content }
         </button>
     );
 };
