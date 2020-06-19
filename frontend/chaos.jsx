a6061b3034b5f4e309d6aa838e54078eb305640a
M3L6H <sisoac@hotmail.com>
2020-06-19 14:02:04 -0700

    Icons can properly act as links now

--- frontend/chaos.jsx
+++ frontend/chaos.jsx
@@ -1,65 +1,9 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 
-import { Button, Icon } from './components/shoebuckle';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<div className="test">
-        <div className="test-row">
-            <Button>Hello World</Button>
-            <Button primary>Hello World</Button>
-            <Button secondary>Hello World</Button>
-            <Button content="Shorthand" primary />
-        </div>
-        <div className="test-row">
-            <Button basic>Hello World</Button>
-            <Button basic red>Hello World</Button>
-            <Button basic orange>Hello World</Button>
-            <Button basic yellow>Hello World</Button>
-            <Button basic olive>Hello World</Button>
-            <Button basic green>Hello World</Button>
-            <Button basic teal>Hello World</Button>
-            <Button basic blue>Hello World</Button>
-            <Button basic violet>Hello World</Button>
-            <Button basic purple>Hello World</Button>
-            <Button basic pink>Hello World</Button>
-            <Button basic grey>Hello World</Button>
-            <Button basic black>Hello World</Button>
-        </div>
-        <div className="test-row inverted">
-            <Button basic inverted>Hello World</Button>
-            <Button basic inverted red>Hello World</Button>
-            <Button basic inverted orange>Hello World</Button>
-            <Button basic inverted yellow>Hello World</Button>
-            <Button basic inverted olive>Hello World</Button>
-            <Button basic inverted green>Hello World</Button>
-            <Button basic inverted teal>Hello World</Button>
-            <Button basic inverted blue>Hello World</Button>
-            <Button basic inverted violet>Hello World</Button>
-            <Button basic inverted purple>Hello World</Button>
-            <Button basic inverted pink>Hello World</Button>
-            <Button basic inverted grey>Hello World</Button>
-            <Button basic inverted black>Hello World</Button>
-        </div>
-        <div className="test-row">
-            <Icon name="briefcase-medical" red mini />
-            <Icon name="briefcase-medical" yellow tiny />
-            <Icon name="briefcase-medical" green small />
-            <Icon name="briefcase-medical" blue />
-            <Icon name="briefcase-medical" purple large />
-            <Icon name="briefcase-medical" pink big />
-            <Icon name="briefcase-medical" grey huge />
-            <Icon name="briefcase-medical" black massive />
-            <Icon name="briefcase-medical" black large disabled />
-            <Icon name="circle-notch" olive large loading />
-            <Icon name="users" teal link />
-            <Icon name="snowboarding" large flipped="horizontal" orange />
-            <Icon name="snowboarding" large flipped="vertical" violet />
-            <Icon name="snowboarding" large flipped="both" teal />
-            <Icon name="snowboarding" large rotated="90" red />
-            <Icon name="snowboarding" large rotated="180" purple />
-            <Icon name="snowboarding" large rotated="270" blue />
-        </div>
-    </div>, root);
+    ReactDOM.render(<Root />, root);
 });
--- frontend/components/shoebuckle/icon.jsx
+++ frontend/components/shoebuckle/icon.jsx
@@ -1,73 +1,98 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withRouter } from "react-router-dom";
 
-export default props => {
-    const {
-        name,
-        loading,
-        flipped,
-        rotated,
-        link,
-        disabled,
-        fitted,
-        inverted,
-        red,
-        orange,
-        yellow,
-        olive,
-        green,
-        teal,
-        blue,
-        violet,
-        purple,
-        pink,
-        grey,
-        black,
-        mini,
-        tiny,
-        small,
-        normal,
-        large,
-        big,
-        huge,
-        massive
-    } = props;
+class Icon extends Component {
+    constructor(props) {
+        super(props);
 
-    const className = `
-        shoebuckle
-        icon
-        fas
-        fa-${ name }
-        ${ loading && "fa-spin" }
-        ${ flipped && `fa-flip-${ flipped }` }
-        ${ rotated && `fa-rotate-${ rotated }` }
-        ${ link && "link" }
-        ${ disabled && "disabled" }
-        ${ fitted && "fitted" }
-        ${ inverted && "inverted" }
-        ${ red && "red" }
-        ${ orange && "orange" }
-        ${ yellow && "yellow" }
-        ${ olive && "olive" }
-        ${ green && "green" }
-        ${ teal && "teal" }
-        ${ blue && "blue" }
-        ${ violet && "violet" }
-        ${ purple && "purple" }
-        ${ pink && "pink" }
-        ${ grey && "grey" }
-        ${ black && "black" }
-        ${ (mini && "mini") ||
-           (tiny && "tiny") ||
-           (small && "small") ||
-           (normal && "normal") ||
-           (large && "large") ||
-           (big && "big") ||
-           (huge && "huge") ||
-           (massive && "massive") ||
-           "normal" }
-    `;
+        this._handleClick = this._handleClick.bind(this);
+    }
+
+    _handleClick() {
+        const { link, to, history } = this.props;
+
+        if (link && to) {
+            history.push(to);
+        }
+    }
+
+    render() {
+        const {
+            children,
+            content,
+            name,
+            loading,
+            flipped,
+            rotated,
+            link,
+            disabled,
+            fitted,
+            inverted,
+            red,
+            orange,
+            yellow,
+            olive,
+            green,
+            teal,
+            blue,
+            violet,
+            purple,
+            pink,
+            grey,
+            black,
+            mini,
+            tiny,
+            small,
+            normal,
+            large,
+            big,
+            huge,
+            massive
+        } = this.props;
     
-    return (
-        <i className={ className }></i>
-    );
-};
+        const className = `
+            shoebuckle
+            icon
+            fas
+            fa-${ name }
+            ${ loading && "fa-spin" }
+            ${ flipped && `fa-flip-${ flipped }` }
+            ${ rotated && `fa-rotate-${ rotated }` }
+            ${ link && "link" }
+            ${ disabled && "disabled" }
+            ${ fitted && "fitted" }
+            ${ inverted && "inverted" }
+            ${ red && "red" }
+            ${ orange && "orange" }
+            ${ yellow && "yellow" }
+            ${ olive && "olive" }
+            ${ green && "green" }
+            ${ teal && "teal" }
+            ${ blue && "blue" }
+            ${ violet && "violet" }
+            ${ purple && "purple" }
+            ${ pink && "pink" }
+            ${ grey && "grey" }
+            ${ black && "black" }
+            ${ (mini && "mini") ||
+               (tiny && "tiny") ||
+               (small && "small") ||
+               (normal && "normal") ||
+               (large && "large") ||
+               (big && "big") ||
+               (huge && "huge") ||
+               (massive && "massive") ||
+               "normal" }
+        `;
+        
+        return (
+            <span className="shoebuckle icon-wrapper" onClick={ this._handleClick }>
+                <i className={ className }>
+                    { children || content }
+                </i>
+            </span>
+        );
+    };
+}
+
+export default withRouter(Icon);
