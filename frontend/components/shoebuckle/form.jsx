d9edef46571b5c7bca64c2c8b8123bc3770bd8c4
M3L6H <sisoac@hotmail.com>
2020-06-22 12:04:24 -0700

    Added form input

--- frontend/components/shoebuckle/form.jsx
+++ frontend/components/shoebuckle/form.jsx
@@ -8,12 +8,69 @@ export default class Form extends Component {
 
     render() {
         const { children, onSubmit } = this.props;
+
+        const className = `shoebuckle form`;
         
         return (
-            <form onSubmit={ onSubmit }>
+            <form onSubmit={ onSubmit } className={ className }>
                 { children }
             </form>
         );
     }
 }
 
+Form.Field = class extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        const { children } = this.props;
+
+        const className = `shoebuckle form-field`;
+
+        return (
+            <div className={ className }>
+                { children }
+            </div>
+        );
+    }
+};
+
+Form.Input = class extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: null
+        };
+
+        this._handleChange = this._handleChange.bind(this);
+    }
+
+    _handleChange(e) {
+        this.setState({ value: e.currentTarget.value });
+    }
+    
+    render() {
+        const { value } = this.state;
+        
+        const { 
+            onChange,
+            placeholder,
+            type 
+        } = this.props;
+
+        const className = `shoebuckle form-input ${ type || "text" }-input`;
+
+        return (
+            <input 
+                className={ className }
+                onChange={ onChange || this._handleChange }
+                placehoder={ placeholder }
+                type={ type }
+                value={ value }
+            />
+        );
+    }
+};
