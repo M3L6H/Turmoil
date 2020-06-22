2da6c99c5dbf46ce6dab94a5162f1675f26c2bb4
M3L6H <sisoac@hotmail.com>
2020-06-22 13:55:56 -0700

    Set up files for messages

--- frontend/components/shoebuckle/index.js
+++ frontend/components/shoebuckle/index.js
@@ -2,3 +2,4 @@ export { default as Button } from "./button";
 export { default as Form } from './form';
 export { default as Icon } from './icon';
 export { default as Label } from './label';
+export { default as Message } from './message';
