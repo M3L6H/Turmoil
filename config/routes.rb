a3e497005943dde1566af38393cb335c00d4c038
M3L6H <sisoac@hotmail.com>
2020-06-23 14:05:34 -0700

    Added roles routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
 
     namespace :api do
         resources :beings, except: [:new, :edit]
-        resources :dimension_beings, only: [:show, :destroy, :update]
+
         resources :dimensions, except: [:new, :edit] do
             resources :dimension_beings, only: [:create]
+            resources :roles, only: [:create]
         end
+
+        resources :dimension_beings, only: [:show, :destroy, :update]
+        resources :roles, only: [:show, :destroy, :update]
         resource :session, only: [:create, :destroy]
     end
 end
