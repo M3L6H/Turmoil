030a6c8f5c6320834c76884804260acb0466b9b1
M3L6H <sisoac@hotmail.com>
2020-06-23 14:28:59 -0700

    Wrote dimension being actions

---
+++ frontend/actions/dimension_beings_actions.js
@@ -0,0 +1,44 @@
+import * as DimensionBeingsUtil from "../util/dimension_beings_util";
+
+export const RECEIVE_DIMENSION_BEING = "RECEIVE_DIMENSION_BEING";
+export const REMOVE_DIMENSION_BEING = "REMOVE_DIMENSION_BEING";
+export const RECEIVE_DIMENSION_BEINGS_ERRORS = "RECEIVE_DIMENSION_BEINGS_ERRORS";
+
+const receiveDimensionBeing = (dimensionBeing) => ({
+    type: RECEIVE_DIMENSION_BEING,
+    dimensionBeing 
+});
+
+const removeDimensionBeing = (dimensionBeingId) => ({
+    type: REMOVE_DIMENSION_BEING,
+    dimensionBeingId
+});
+
+const receiveDimensionBeingsErrors = (errors) => ({
+    type: RECEIVE_DIMENSION_BEINGS_ERRORS,
+    dimensionBeings: errors
+});
+
+export const fetchDimensionBeing = dimensionBeingId => dispatch => (
+    DimensionBeingsUtil.fetchDimensionBeing(dimensionBeingId)
+        .then(res => dispatch(receiveDimensionBeing(res)))
+        .fail(jqXHR => receiveDimensionBeingsErrors(jqXHR.responseJSON))
+);
+
+export const createDimensionBeing = (dimensionBeing, dimensionId) => dispatch => (
+    DimensionBeingsUtil.createDimensionBeing(dimensionBeing, dimensionId)
+        .then(res => dispatch(receiveDimensionBeing(res)))
+        .fail(jqXHR => receiveDimensionBeingsErrors(jqXHR.responseJSON))
+);
+
+export const deleteDimensionBeing = dimensionBeingId => dispatch => (
+    DimensionBeingsUtil.deleteDimensionBeing(dimensionBeingId)
+        .then(res => dispatch(removeDimensionBeing(res)))
+        .fail(jqXHR => receiveDimensionBeingsErrors(jqXHR.responseJSON))
+);
+
+export const updateDimensionBeing = dimensionBeing => dispatch => (
+    DimensionBeingsUtil.updateDimensionBeing(dimensionBeing)
+        .then(res => dispatch(receiveDimensionBeing(res)))
+        .fail(jqXHR => receiveDimensionBeingsErrors(jqXHR.responseJSON))
+);
