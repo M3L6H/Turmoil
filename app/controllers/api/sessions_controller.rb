809dc212de4e6e2a0e8fe6dece8a77de85747ca4
M3L6H <sisoac@hotmail.com>
2020-06-22 17:45:36 -0700

    Added truncated C.R.L.L.L

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,3 @@
 class Api::SessionsController < ApplicationController
+    
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login(user)
+        session[:session_token] = user.reset_session_token!
+        @current_user = user
+    end
+
+    def logout
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def logged_in?
+        !!current_user
+    end
 
     def being_params
         params.require(:user).permit(:username, :email, :password)
