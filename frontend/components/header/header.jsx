6fc7e9e41ddf88ed91e31ce39d243653e0fec5e2
M3L6H <sisoac@hotmail.com>
2020-06-22 23:15:38 -0700

    Added header

---
+++ frontend/components/header/header.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+    renderAuthButtons() {
+        
+    }
+    
+    render() {
+        return (
+            <header>
+                <h2>Chaos</h2>
+                <nav>
+                    <ul>
+                    </ul>
+                </nav>
+            </header>
+        );
+    }
+}
