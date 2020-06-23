1d449add126846b36031088d0d30fdc19bd77d08
M3L6H <sisoac@hotmail.com>
2020-06-23 09:14:27 -0700

    Added being seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Being.destroy_all
+
+catlover = Being.create!(username: "catlover", password: "catlover123", email: "catlover23@cats.co")
+l33t = Being.create!(username: "l33t", password: "c0rndog$$", email: "jill_hamilton@people.net")
+cerberus = Being.create!(username: "cerberus", password: "!demondogpuppies!", email: "cerb@hell.com")
+gandalf111 = Being.create!(username: "gandalf111", password: "YOUSHAlL N0T P@SS", email: "gandalf111@wizards.io")
+rocket = Being.create!(username: "rocket", password: "cefG-hyxg-fWWv", email: "rocket_is@raccoon.gov")
+not_markiplier = Being.create!(username: "NotMarkiplier", password: "h@xx_D1s_if_U_cann!!?>>", email: "Mark.Sharkbait@personalities.com")
+spiderman = Being.create!(username: "Spiderman", password: "itsy bitsy spidy crawled up the waterspoit", email: "peter.parker@marvel.org")
+justin_y = Being.create!(username: "JustinY", password: "i'm 3verywh3re youl00k", email: "TheJustinY@anonymous.io")
+akali1987 = Being.create!(username: "Akali1987", password: "jhRs-OXzi-aMlp", email: "akali@league.com")
+erza_scarlet = Being.create!(username: "Erza_Scarlet", password: "jellal is overrated", email: "scarlet123@fairytail.com")
