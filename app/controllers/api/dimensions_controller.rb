7555b0fda4823b3ad657b8a363b87700a35edb3c
M3L6H <sisoac@hotmail.com>
2020-06-23 10:47:35 -0700

    Created dimension views and dimension controller index action

--- app/controllers/api/dimensions_controller.rb
+++ app/controllers/api/dimensions_controller.rb
@@ -1,5 +1,14 @@
 class Api::DimensionsController < ApplicationController
+    before_action :require_logged_in, only: [:create]
+    
     def index
+        respond_to do |format|
+            format.html { redirect_back fallback_location: "/" }
+            format.json do
+                @dimensions = Dimension.where(public: true)
+                render :index
+            end
+        end
     end
 
     def show
