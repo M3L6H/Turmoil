c07a6b7afb62ecd52a04abb42cd08b57d7c080f5
M3L6H <sisoac@hotmail.com>
2020-06-23 00:53:37 -0700

    Controlled form!

--- frontend/components/auth/auth_form.jsx
+++ frontend/components/auth/auth_form.jsx
@@ -5,36 +5,85 @@ import { Button, Form, Modal } from '../shoebuckle';
 export default class AuthForm extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            username: "",
+            email: "",
+            password: ""
+        };
     
+        this._handleChange = this._handleChange.bind(this);
         this._handleClose = this._handleClose.bind(this);
         this._handleSubmit = this._handleSubmit.bind(this);
+        this._renderForm = this._renderForm.bind(this);
+    }
+
+    _handleChange(e) {
+        const input = e.currentTarget;
+
+        switch (input.dataset.type) {
+            case "username":
+            case "email":
+            case "password":
+                this.setState({ [input.dataset.type]: input.value });
+        }
     }
     
     _handleClose() {
         this.props.closeForm();
     }
 
-    _handleSubmit(being) {
+    _handleSubmit(e) {
+        e.preventDefault();
+    }
 
+    _renderForm(title) {
+        const { username, email, password } = this.state;
+        const { formType } = this.props;
+        
+        const placeholder = formType === "signIn" ? "Username or Email" : "Username";
+        const label = formType === "signIn" ? "Who goes there?" : "State your name, mortal";
+
+        return (
+            <Form onSubmit={ this._handleSubmit }>
+                <Form.Input 
+                    placeholder={ placeholder } 
+                    label={ label } 
+                    data-type="username"
+                    onChange={ this._handleChange }
+                    value={ username }
+                />
+                { formType === "signUp" && (
+                    <Form.Input 
+                        placeholder="Email" 
+                        label="How may we contact you?" 
+                        data-type="email"
+                        onChange={ this._handleChange }
+                        value={ email }
+                    />
+                )}
+                <Form.Input 
+                    type="password" 
+                    label="What is the password?" 
+                    data-type="password"
+                    onChange={ this._handleChange }
+                    value={ password }
+                />
+                <Button fluid green type="submit" content={ title } />
+            </Form>
+        );
     }
     
     render() {
-        const { formType, open } = this.props;
-        
+        const { open, formType } = this.props;
+
         const title = formType === "signIn" ? "Sign In" : "Sign Up";
 
         return (
             <Modal handleClose={ this._handleClose } open={ open }>
                 <Modal.Header>{ title }</Modal.Header>
                 <Modal.Content>
-                    <Form onSubmit={ this._handleSubmit }>
-                        <Form.Input placeholder="Username" label="State your name, mortal" />
-                        { formType === "signUp" && (
-                            <Form.Input placeholder="Email" label="How may we contact you?" />
-                        )}
-                        <Form.Input type="password" label="What is the password?" />
-                        <Button fluid green type="submit" content={ title } />
-                    </Form>
+                    { this._renderForm(title) }
                 </Modal.Content>
             </Modal>
         );
