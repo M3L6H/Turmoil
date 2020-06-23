5e63b71cdb498a905ed6cb4ecc2b0f34fdfe8259
M3L6H <sisoac@hotmail.com>
2020-06-22 18:34:02 -0700

    Created store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,3 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers/root_reducer';
+
+export default (preloadedState) => (
+    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
+);
