85fc4f5fc605cdd63eb94f6ac31af7eb866ce5a8
M3L6H <sisoac@hotmail.com>
2020-06-23 10:43:35 -0700

    Generated dimensions controller

---
+++ app/controllers/api/dimensions_controller.rb
@@ -0,0 +1,16 @@
+class Api::DimensionsController < ApplicationController
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+
+    def update
+    end
+end
