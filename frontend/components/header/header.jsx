48da8d1100d7e9dddc2d2e96b4abf53f8ebaa092
M3L6H <sisoac@hotmail.com>
2020-06-22 23:45:01 -0700

    Basic header going

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 
+import { Button } from '../shoebuckle';
+
 export default class Header extends Component {
-    renderAuthButtons() {
-        
+    constructor(props) {
+        super(props)
+    
+        this._renderAuthButtons = this._renderAuthButtons.bind(this);
+    }
+    
+    
+    _renderAuthButtons() {
+        const {
+            currentBeingId,
+            openSignIn,
+            closeSignIn,
+            openSignUp,
+            closeSignUp,
+            logout
+        } = this.props;
+
+        if (currentBeingId) {
+            return (
+                <li><Button pill onClick={ signOut }>Sign Out</Button></li>
+            );
+        } else {
+            return (
+                <>
+                    <li><Button pill onClick={ openSignIn }>Sign In</Button></li>
+                    <li><Button pill onClick={ openSignUp }>Sign Up</Button></li>
+                </>
+            );
+        }
     }
     
     render() {
+        
         return (
             <header>
                 <h2>Chaos</h2>
                 <nav>
                     <ul>
+                        { this._renderAuthButtons() }
                     </ul>
                 </nav>
             </header>
--- frontend/components/header/header_container.js
+++ frontend/components/header/header_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 
 import { receiveAuthModal } from '../../actions/modals_actions';
+import { deleteSession } from '../../actions/session_actions';
 
 import Header from './header';
 
@@ -14,7 +15,8 @@ const mapDispatchToProps = (dispatch) => ({
     openSignUp: () => dispatch(receiveAuthModal(true, "signUp")),
     closeSignUp: () => dispatch(receiveAuthModal(false, "signUp")),
     openSignIn: () => dispatch(receiveAuthModal(true, "signIn")),
-    closeSignIn: () => dispatch(receiveAuthModal(false, "signIn"))
+    closeSignIn: () => dispatch(receiveAuthModal(false, "signIn")),
+    signOut: () => dispatch(deleteSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
