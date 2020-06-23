d83f7496a715b2004dea9798988105433afebe11
M3L6H <sisoac@hotmail.com>
2020-06-23 14:55:24 -0700

    Added files for shoebuckle header

--- frontend/components/shoebuckle/index.js
+++ frontend/components/shoebuckle/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from "./button";
 export { default as Form } from './form';
+export { default as Header } from './header';
 export { default as Icon } from './icon';
 export { default as Label } from './label';
 export { default as Message } from './message';
