0b23a0654465be658351d36cf3aa34be173db30f
M3L6H <sisoac@hotmail.com>
2020-06-23 10:58:56 -0700

    Added show and create to dimension actions

--- app/controllers/api/dimensions_controller.rb
+++ app/controllers/api/dimensions_controller.rb
@@ -1,25 +1,42 @@
 class Api::DimensionsController < ApplicationController
+    before_action :require_json
     before_action :require_logged_in, only: [:create]
     
     def index
-        respond_to do |format|
-            format.html { redirect_back fallback_location: "/" }
-            format.json do
-                @dimensions = Dimension.where(public: true)
-                render :index
-            end
-        end
+        # TODO: Update with query parameters
+        @dimensions = Dimension.where(public: true)
+        render :index
     end
 
     def show
+        @dimension = Dimension.find_by(id: params[:id])
+
+        if @dimension
+            render :show
+        else
+            render json: ["Could not find dimension with id #{params[:id]}"], status: 404
+        end
     end
 
     def create
+        @dimension = Dimension.new(dimension_params)
+        @dimension.being_id = current_being.id
+
+        if @dimension.save
+            render :create
+        else
+            render json: @dimension.errors.full_messages, status: 422
+        end
     end
 
     def destroy
     end
 
     def update
+    end
+
+private
+    def dimension_params
+        params.require(:dimension).permit(:name, :public)
     end
 end
