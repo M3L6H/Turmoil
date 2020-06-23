6fd65ee8e246b1514d3e1648746ac743ab307924
M3L6H <sisoac@hotmail.com>
2020-06-23 11:31:31 -0700

    Created dimension actions

---
+++ frontend/actions/dimensions_actions.js
@@ -0,0 +1,56 @@
+import * as DimensionsUtil from "../util/dimensions_util";
+
+export const RECEIVE_DIMENSIONS = "RECEIVE_DIMENSIONS";
+export const RECEIVE_DIMENSION = "RECEIVE_DIMENSION";
+export const REMOVE_DIMENSION = "REMOVE_DIMENSION";
+export const RECEIVE_DIMENSIONS_ERRORS = "RECEIVE_DIMENSIONS_ERRORS";
+
+const receiveDimensions = (dimensions) => ({
+    type: RECEIVE_DIMENSIONS,
+    dimensions 
+});
+
+const receiveDimension = (dimension) => ({
+    type: RECEIVE_DIMENSION,
+    dimension 
+});
+
+const removeDimension = (dimensionId) => ({
+    type: REMOVE_DIMENSION,
+    dimensionId
+});
+
+const receiveDimensionsErrors = (errors) => ({
+    type: RECEIVE_DIMENSIONS_ERRORS,
+    dimensions: errors
+});
+
+export const fetchDimensions = () => dispatch => (
+    DimensionsUtil.fetchDimensions()
+        .then(res => dispatch(receiveDimensions(res)))
+        .fail(jqXHR => receiveDimensionsErrors(jqXHR.responseJSON))
+);
+
+export const fetchDimension = (dimensionId) => dispatch => (
+    DimensionsUtil.fetchDimension(dimensionId)
+        .then(res => dispatch(receiveDimension(res)))
+        .fail(jqXHR => receiveDimensionsErrors(jqXHR.responseJSON))
+);
+
+export const createDimension = (dimension) => dispatch => (
+    DimensionsUtil.createDimension(dimension)
+        .then(res => dispatch(receiveDimension(res)))
+        .fail(jqXHR => receiveDimensionsErrors(jqXHR.responseJSON))
+);
+
+export const deleteDimension = (dimensionId) => dispatch => (
+    DimensionsUtil.deleteDimension(dimensionId)
+        .then(res => dispatch(removeDimension(res)))
+        .fail(jqXHR => receiveDimensionsErrors(jqXHR.responseJSON))
+);
+
+export const updateDimension = (dimension) => dispatch => (
+    DimensionsUtil.updateDimension(dimension)
+        .then(res => dispatch(receiveDimension(res)))
+        .fail(jqXHR => receiveDimensionsErrors(jqXHR.responseJSON))
+);
