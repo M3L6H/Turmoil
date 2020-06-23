d006028075d9b29311fec9d5e20a633ebdecac7c
M3L6H <sisoac@hotmail.com>
2020-06-22 17:48:13 -0700

    Created sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,3 +1,22 @@
 class Api::SessionsController < ApplicationController
-    
+    def create
+        params = user_params
+        @user = User.find_by_credentials(params.username, params.password)
+
+        if @user
+            login(@user)
+            render :create
+        else
+            render json: ["Invalid credentials"], status: 422
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout
+            render json: {}, status: 200
+        else
+            render json: ["Not logged in"], status: 403
+        end
+    end
 end
