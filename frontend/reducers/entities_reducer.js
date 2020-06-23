4ab42be3815f1ee86550ef0bdf1b5b19966f5a65
M3L6H <sisoac@hotmail.com>
2020-06-23 14:42:23 -0700

    Hooked into entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -2,8 +2,12 @@ import { combineReducers } from 'redux';
 
 import beingsReducer from './beings_reducer';
 import dimensionsReducer from './dimensions_reducer';
+import dimensionBeingsReducer from './dimension_beings_reducer';
+import rolesReducer from './roles_reducer';
 
 export default combineReducers({
     beings: beingsReducer,
-    dimensions: dimensionsReducer
+    dimensions: dimensionsReducer,
+    dimensionBeings: dimensionBeingsReducer,
+    roles: rolesReducer
 });
