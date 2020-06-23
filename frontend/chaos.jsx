3bce945687c675cfad054a222b1624975992290a
M3L6H <sisoac@hotmail.com>
2020-06-22 18:37:22 -0700

    Hooked store into app

--- frontend/chaos.jsx
+++ frontend/chaos.jsx
@@ -3,7 +3,13 @@ import ReactDOM from "react-dom";
 
 import Root from './components/root';
 
+import configureStore from './store/store';
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    ReactDOM.render(<Root />, root);
+    const store = configureStore();
+    ReactDOM.render(<Root store={ store } />, root);
+
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
 });
--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 import App from './app';
 
 const Root = ({ store }) => (
-    // <Provider store={ store }>
+    <Provider store={ store }>
         <HashRouter>
             <App />
         </HashRouter>
-    // </Provider>
+    </Provider>
 );
 
 export default Root;
