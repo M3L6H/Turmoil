b8da0ea109a24e6e7d3f5b4d2e3fb23a85f88493
M3L6H <sisoac@hotmail.com>
2020-06-24 09:24:19 -0700

    Connected sidebar state to app container

--- frontend/components/app_container/app_container_container.js
+++ frontend/components/app_container/app_container_container.js
@@ -2,12 +2,15 @@ import { connect } from 'react-redux';
 
 import AppContainer from './app_container';
 
+import { receiveSidebar } from '../../actions/sidebar_actions';
+
 const mapStateToProps = (state) => ({
-    
+    ...state.ui.sidebar
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+    openSidebar: () => dispatch(receiveSidebar({ open: true })),
+    closeSidebar: () => dispatch(receiveSidebar({ open: false }))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(AppContainer);
