bc4d9f549c7dfdf17b15e8ed16ae195f3fc09f68
M3L6H <sisoac@hotmail.com>
2020-06-25 14:23:42 -0700

    Added both clusters reducer and realms reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,13 +1,17 @@
 import { combineReducers } from 'redux';
 
 import beingsReducer from './beings_reducer';
+import clustersReducer from './clusters_reducer';
 import dimensionsReducer from './dimensions_reducer';
 import dimensionBeingsReducer from './dimension_beings_reducer';
+import realmsReducer from './realms_reducer';
 import rolesReducer from './roles_reducer';
 
 export default combineReducers({
     beings: beingsReducer,
+    clusters: clustersReducer,
     dimensions: dimensionsReducer,
     dimensionBeings: dimensionBeingsReducer,
+    realms: realmsReducer,
     roles: rolesReducer
 });
