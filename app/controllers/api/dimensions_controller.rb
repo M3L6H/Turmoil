1ea36c6d5f5319a86af220e4c66938087afa1e52
M3L6H <sisoac@hotmail.com>
2020-06-25 16:13:37 -0700

    Got orderables working.

--- app/controllers/api/dimensions_controller.rb
+++ app/controllers/api/dimensions_controller.rb
@@ -24,13 +24,15 @@ class Api::DimensionsController < ApplicationController
 
         if @dimension.save
             # Initialize the details of a dimension
-            Role.create!(dimension_id: @dimension.id, can_be_deleted: false, name: "everyone")
-            DimensionBeing.create!(nickname: current_being.username, being_id: current_being.id, dimension_id: @dimension.id)
-            text_cluster = Cluster.create!(name: "Text Realms", dimension_id: @dimension.id)
-            voice_cluster = Cluster.create!(name: "Voice Realms", dimension_id: @dimension.id, prev_orderable_id: text_cluster.id, prev_orderable_type: "Cluster")
-            text_cluster.update!(next_orderable_id: voice_cluster.id, next_orderable_type: "Cluster")
-            Realm.create!(name: "general", cluster_id: text_cluster.id, dimension_id: @dimension.id)
-            Realm.create!(name: "general", cluster_id: voice_cluster.id, dimension_id: @dimension.id, realm_type: "voice")
+            Dimension.transaction do
+                Role.create!(dimension_id: @dimension.id, can_be_deleted: false, name: "everyone")
+                DimensionBeing.create!(nickname: current_being.username, being_id: current_being.id, dimension_id: @dimension.id)
+                text_cluster = Cluster.create!(name: "Text Realms", dimension_id: @dimension.id)
+                voice_cluster = Cluster.create!(name: "Voice Realms", dimension_id: @dimension.id, prev_orderable_id: text_cluster.id, prev_orderable_type: "Cluster")
+                text_cluster.update!(next_orderable_id: voice_cluster.id, next_orderable_type: "Cluster")
+                Realm.create!(name: "general", cluster_id: text_cluster.id, dimension_id: @dimension.id)
+                Realm.create!(name: "general", cluster_id: voice_cluster.id, dimension_id: @dimension.id, realm_type: "voice")
+            end
             render :create
         else
             render json: @dimension.errors.full_messages, status: 422
