b48e356854dca921a7b02f5fc4ffb6638616ab7f
M3L6H <sisoac@hotmail.com>
2020-06-24 18:00:02 -0700

    Updated viewport height and width live

--- frontend/components/hocs/with_window_dimensions.jsx
+++ frontend/components/hocs/with_window_dimensions.jsx
@@ -26,6 +26,12 @@ export default (WrappedComponent) => {
                 windowHeight: window.innerHeight,
                 desktop: window.innerWidth > BREAKPOINT
             });
+
+            // Update viewport height and width 
+            const vh = window.innerHeight * 0.01;
+            const vw = window.innerWidth * 0.01;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+            document.documentElement.style.setProperty("--vw", `${vw}px`);
         }
 
         render() {
