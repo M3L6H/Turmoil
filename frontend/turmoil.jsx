c590b80891c4007cecb8ee4cc64fa5df7a291996
M3L6H <sisoac@hotmail.com>
2020-06-24 17:58:05 -0700

    Added custom css variables for viewport height and width

--- frontend/turmoil.jsx
+++ frontend/turmoil.jsx
@@ -29,6 +29,12 @@ document.addEventListener("DOMContentLoaded", () => {
         store = configureStore();
     }
 
+    // Set up viewport height and width for mobile
+    const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--vw", `${vw}px`);
+
     const root = document.getElementById("root");
     ReactDOM.render(<Root store={ store } />, root);
 
