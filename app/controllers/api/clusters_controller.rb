be8c042e44d36aa9f378f0a656c827dbc323dc5d
M3L6H <sisoac@hotmail.com>
2020-06-25 12:59:23 -0700

    Wrote up clusters controller

---
+++ app/controllers/api/clusters_controller.rb
@@ -0,0 +1,44 @@
+class Api::ClustersController < ApplicationController
+    def create
+        @cluster = Cluster.new(cluster_params)
+
+        if @cluster.save
+            # TODO: Only beings with the right permissions should be able to create clusters
+            render :create
+        else
+            render json: @cluster.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @cluster = Cluster.find_by(id: params[:id])
+
+        if @cluster
+            # TODO: Only beings with the right permissions should be able to destroy clusters
+            @cluster.destroy
+            render :destroy
+        else
+            render json: ["Could not find cluster with id #{ params[:id] }"], status: 404
+        end
+    end
+
+    def update
+        @cluster = Cluster.find_by(id: params[:id])
+
+        if @cluster
+            # TODO: Only beings with the right permissions should be able to update clusters
+            if @cluster.update(cluster_params)]
+                render :update
+            else
+                render json: @cluster.errors.full_messages, status: 422
+            end
+        else
+            render json: ["Could not find cluster with id #{ params[:id] }"], status: 404
+        end
+    end
+
+private
+    def cluster_params
+        params.require(:cluster).permit(:name, :private, :permitted_roles, :dimension_id)
+    end
+end
