ebe6f0745a287787b6588a6dd249e95856ca644e
M3L6H <sisoac@hotmail.com>
2020-06-25 14:21:42 -0700

    Added cluster actions

---
+++ frontend/actions/clusters_actions.js
@@ -0,0 +1,38 @@
+import * as ClustersUtil from "../util/clusters_util";
+
+export const RECEIVE_CLUSTER = "RECEIVE_CLUSTER";
+export const REMOVE_CLUSTER = "REMOVE_CLUSTER";
+export const RECEIVE_CLUSTERS_ERRORS = "RECEIVE_CLUSTERS_ERRORS";
+
+const receiveCluster = ({ cluster }) => ({
+    type: RECEIVE_CLUSTER,
+    cluster
+});
+
+const removeCluster = (clusterId) => ({
+    type: REMOVE_CLUSTER,
+    clusterId
+});
+
+const receiveClustersErrors = (errors) => ({
+    type: RECEIVE_CLUSTERS_ERRORS,
+    clusters: errors
+});
+
+export const createCluster = cluster => dispatch => (
+    ClustersUtil.createCluster(cluster)
+        .then(res => dispatch(receiveCluster(res)))
+        .fail(jqXHR => receiveClustersErrors(jqXHR.responseJSON))
+);
+
+export const deleteCluster = clusterId => dispatch => (
+    ClustersUtil.deleteCluster(clusterId)
+        .then(res => dispatch(removeCluster(res)))
+        .fail(jqXHR => receiveClustersErrors(jqXHR.responseJSON))
+);
+
+export const updateCluster = cluster => dispatch => (
+    ClustersUtil.updateCluster(cluster)
+        .then(res => dispatch(receiveCluster(res)))
+        .fail(jqXHR => receiveClustersErrors(jqXHR.responseJSON))
+);
