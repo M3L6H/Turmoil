ca2afc9f00c29eda1cf879b70f0524f7f577767d
M3L6H <sisoac@hotmail.com>
2020-06-26 15:26:23 -0700

    Refactoring in preparation for moving items in and out of clusters

--- frontend/components/realms/realm_menu.jsx
+++ frontend/components/realms/realm_menu.jsx
@@ -19,34 +19,41 @@ export default class RealmMenu extends Component {
         this.props.select(dragNDropItem.id.split("-")[1]);
     }
 
+    // Convert clusters takes the clusters we get from the database and turns it
+    // into a format that our dnd component can use
+    // This method could have been put in a reducer, but its sibling,
+    // convertRealms, could not, so for consistency we leave this here
     _convertClusters() {
         const { clusters } = this.props;
-        const converted = {};
+        const converted = [];
 
         for (const id in clusters) {
             const cluster = clusters[id];
-            converted[`Cluster-${ id }`] = {
+            converted.push({
                 id: `Cluster-${ id }`,
                 content: cluster.name,
                 type: "folder",
                 next: cluster.nextOrderableId ? `${ cluster.nextOrderableType }-${ cluster.nextOrderableId }` : null,
                 prev: cluster.prevOrderableId ? `${ cluster.prevOrderableType }-${ cluster.prevOrderableId }` : null,
-                parent: null
-            };
+                parent: "root"
+            });
         }
 
         return converted;
     }
 
+    // Like convert clusters above, this method converts realms into a format
+    // that our dnd component can use. This method has to be in this class since
+    // it needs to reference the onClick event handler selectRealm
     _convertRealms() {
         const { realms } = this.props;
-        const converted = {};
+        const converted = [];
 
         for (const id in realms) {
             const realm = realms[id];
             const icon = realm.realmType === "text" ? "hashtag" : "microphone";
-            const parent = realm.clusterId ? `Cluster-${ realm.clusterId }` : null;
-            converted[`Realm-${ id }`] = {
+            const parent = realm.clusterId ? `Cluster-${ realm.clusterId }` : "root";
+            converted.push({
                 id: `Realm-${ id }`,
                 content: <Menu.Item justifyStart><Icon name={ icon } /> { realm.name }</Menu.Item>,
                 onClick: this._selectRealm,
@@ -54,38 +61,53 @@ export default class RealmMenu extends Component {
                 next: realm.nextOrderableId ? `${ realm.nextOrderableType }-${ realm.nextOrderableId }` : null,
                 prev: realm.prevOrderableId ? `${ realm.prevOrderableType }-${ realm.prevOrderableId }` : null,
                 parent
-            };
+            });
         }
 
         return converted;
     }
 
+    // This constructs a linked list out of the data we receive from our server
+    // It also uses a lookupTable to track the different linked lists that get
+    // created so we can support movement between lists
     _constructList() {
-        const clusters = this._convertClusters();
-        const realms = this._convertRealms();
-        
-        const clustersArray = Object.values(clusters);
-        const realmsArray = Object.values(realms);
-        const rootRealms = realmsArray.filter(({ parent }) => !parent);
+        // We first conver the clusters and realms into the right format for our
+        // dnd component
+        const clustersArray = this._convertClusters();
+        const realmsArray = this._convertRealms();
+
+        // Then we grab all the realms that are not in a cluster
+        const rootRealms = realmsArray.filter(({ parent }) => parent === "root");
+
+        // We turn those along with the clusters into a linked list...
         const ll = new LinkedList(clustersArray.concat(rootRealms));
+
+        // ...and save that linked list to our lookupTable for later use
         this.lookupTable["root"] = ll;
 
+        // Then we go through our clusters and find all the realms it contains
         clustersArray.forEach(cluster => {
             const clusterRealms = realmsArray.filter(({ parent }) => parent === cluster.id);
             const subLl = new LinkedList(clusterRealms);
+            // Again we save the linked list to our lookupTable
             this.lookupTable[clusterRealms.id] = subLl;
+            // We also update the original linked list item
             ll.updateItem(cluster.id, { children: subLl });
         });
 
+        // Finally we return the root list we generated
         return ll;
     }
 
+    // Move before moves the item with id itemId before the item with id
+    // beforeId. If beforeId is null, it appends to the list instead.
+    // fromListId and toListId optionally specify which lists to find and place
+    // the item in.
     _moveBefore(beforeId, itemId, fromListId="root", toListId="root") {
         const fromList = this.lookupTable[fromListId];
         const toList = this.lookupTable[toListId];
-        this.lookupTable[itemId] = toList;
         const item = fromList.removeItem(itemId);
-        toList.insertItemBefore(beforeId, item);
+        toList.insertItemBefore(beforeId, { ...item, parent: toListId });
     }
     
     render() {
