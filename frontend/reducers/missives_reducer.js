a84f0531ce90d31af3919b74948b665aed445926
M3L6H <sisoac@hotmail.com>
2020-06-28 14:57:29 -0700

    Created missives reducer

---
+++ frontend/reducers/missives_reducer.js
@@ -0,0 +1,18 @@
+import { CLEAR_MISSIVES, RECEIVE_MISSIVE } from '../actions/missives_actions';
+
+import { RECEIVE_REALM } from '../actions/realms_actions';
+
+export default (state={}, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case CLEAR_MISSIVES:
+            return {};
+        case RECEIVE_REALM:
+            return action.missives;
+        case RECEIVE_MISSIVE:
+            return { ...state, [action.missive.id]: action.missive };
+        default:
+            return state;
+    }
+};
