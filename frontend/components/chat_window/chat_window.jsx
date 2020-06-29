066199678a87f8d53df34c37a30a090f2bc52bd5
M3L6H <sisoac@hotmail.com>
2020-06-28 22:41:00 -0700

    Wrote handle submit for chat window

--- frontend/components/chat_window/chat_window.jsx
+++ frontend/components/chat_window/chat_window.jsx
@@ -11,6 +11,7 @@ export default class ChatWindow extends Component {
         };
 
         this._handleChange = this._handleChange.bind(this);
+        this._handleSubmit = this._handleSubmit.bind(this);
     }
     
     _handleChange(e) {
@@ -19,6 +20,22 @@ export default class ChatWindow extends Component {
         switch(input.dataset.type) {
             case "body":
                 this.setState({ [input.dataset.type]: input.value });
+        }
+    }
+
+    _handleSubmit() {
+        const { body } = this.state;
+        const { selectedRealm } = this.props;
+        
+        if (body !== "") {
+            const missive = { 
+                body,
+                messageable_id: selectedRealm.id,
+                messageable_type: "Realm"
+            };
+
+            this.props.createMissive(missive);
+            this.setState({ body: "" });
         }
     }
     
@@ -36,16 +53,18 @@ export default class ChatWindow extends Component {
         
         return (
             <Section className="chat-window" inverted={ inverted }>
-                <Form className="chat-bar">
+                <Form className="chat-bar" onSubmit={ this._handleSubmit }>
                     <Form.Input 
                         dataset-type="body" 
                         placeholder={ `message #${ selectedRealm.name }` }
                         value={ body }
                         onChange={ this._handleChange } 
                     />
-                    <Button type="submit" icon>
-                        <Icon name="arrow-alt-circle-right" primary large />
-                    </Button>
+                    { body !== "" && (
+                        <Button type="submit" icon>
+                            <Icon name="arrow-alt-circle-right" primary large />
+                        </Button>
+                    ) }
                 </Form>
             </Section>
         );
