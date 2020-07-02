fa8a6435d68fa4c4252c2ac60396f87e2b762512
M3L6H <sisoac@hotmail.com>
2020-07-01 18:11:33 -0700

    Padded out numbers

--- frontend/components/missive/missive.jsx
+++ frontend/components/missive/missive.jsx
@@ -10,12 +10,13 @@ export default props => {
     const date = new Date(createdAt || created_at);
     const now = new Date();
     let dateString = date.toLocaleDateString("en-US");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
 
     if (date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()) {
         if (date.getDate() === now.getDate()) {
-            dateString = `Today at ${ date.getHours() % 12 }:${ date.getMinutes() } ${ date.getHours() < 12 ? "AM" : "PM" }`;
+            dateString = `Today at ${ date.getHours() % 12 }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
         } else if (date.getDate() + 1 === now.getDate()) {
-            dateString = `Yesterday at ${ date.getHours() % 12 }:${ date.getMinutes() } ${ date.getHours() < 12 ? "AM" : "PM" }`;
+            dateString = `Yesterday at ${ date.getHours() % 12 }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
         }
     }
 
