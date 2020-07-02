b2b59c6d27ab26f085f8f013ff9e1e4ac96374ae
M3L6H <sisoac@hotmail.com>
2020-07-02 00:15:22 -0700

    Made edit realm form

---
+++ frontend/components/realms/edit_realm_form_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+
+import EditRealmForm from './edit_realm_form';
+
+const mapStateToProps = (state) => ({
+    
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditRealmForm);
