a8a21e22d8b1cd75ec95480c855763dd099cdf96
M3L6H <sisoac@hotmail.com>
2020-07-02 00:21:48 -0700

    Fixed chat window styling

--- frontend/components/missive/missive.jsx
+++ frontend/components/missive/missive.jsx
@@ -10,13 +10,15 @@ export default props => {
     const date = new Date(createdAt || created_at);
     const now = new Date();
     let dateString = date.toLocaleDateString("en-US");
+    let hours = date.getHours() % 12;
+    hours = hours === 0 ? 12 : hours;
     const minutes = date.getMinutes().toString().padStart(2, "0");
 
     if (date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()) {
         if (date.getDate() === now.getDate()) {
-            dateString = `Today at ${ date.getHours() % 12 }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
+            dateString = `Today at ${ hours }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
         } else if (date.getDate() + 1 === now.getDate()) {
-            dateString = `Yesterday at ${ date.getHours() % 12 }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
+            dateString = `Yesterday at ${ hours }:${ minutes } ${ date.getHours() < 12 ? "AM" : "PM" }`;
         }
     }
 
