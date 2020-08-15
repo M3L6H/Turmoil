05416f44ac5d233622bf6b29ced56f38cf45a176
M3L6H <sisoac@hotmail.com>
2020-08-15 00:11:03 -0700

    Update beings actions to receive comrades

--- frontend/actions/beings_actions.js
+++ frontend/actions/beings_actions.js
@@ -6,55 +6,57 @@ export const REMOVE_BEING = "REMOVE_BEING";
 export const RECEIVE_BEINGS_ERRORS = "RECEIVE_BEINGS_ERRORS";
 
 const receiveBeings = (beings) => ({
-    type: RECEIVE_BEINGS,
-    beings 
+  type: RECEIVE_BEINGS,
+  beings 
 });
 
-const receiveBeing = ({ being, dimensions }) => ({
-    type: RECEIVE_BEING,
-    being,
-    dimensions,
-    beingId: being.id
+const receiveBeing = ({ being, dimensions, comrades, comradeBeings }) => ({
+  type: RECEIVE_BEING,
+  being,
+  dimensions,
+  beingId: being.id,
+  comrades,
+  comradeBeings
 });
 
 const removeBeing = (beingId) => ({
-    type: REMOVE_BEING,
-    beingId
+  type: REMOVE_BEING,
+  beingId
 });
 
 const receiveBeingsErrors = (errors) => ({
-    type: RECEIVE_BEINGS_ERRORS,
-    beings: errors
+  type: RECEIVE_BEINGS_ERRORS,
+  beings: errors
 });
 
 export const fetchBeings = () => dispatch => (
-    BeingsUtil.fetchBeings()
-        .then(res => dispatch(receiveBeings(res)))
-        .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
+  BeingsUtil.fetchBeings()
+    .then(res => dispatch(receiveBeings(res)))
+    .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
 );
 
 export const fetchBeing = (beingId) => dispatch => (
-    BeingsUtil.fetchBeing(beingId)
-        .then(res => dispatch(receiveBeing(res)))
-        .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
+  BeingsUtil.fetchBeing(beingId)
+    .then(res => dispatch(receiveBeing(res)))
+    .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
 );
 
 export const createBeing = (being) => dispatch => (
-    BeingsUtil.createBeing(being)
-        .then(res => {
-            dispatch(receiveBeing(res));
-        })
-        .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
+  BeingsUtil.createBeing(being)
+    .then(res => {
+      dispatch(receiveBeing(res));
+    })
+    .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
 );
 
 export const deleteBeing = (beingId) => dispatch => (
-    BeingsUtil.deleteBeing(beingId)
-        .then(res => dispatch(removeBeing(res)))
-        .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
+  BeingsUtil.deleteBeing(beingId)
+    .then(res => dispatch(removeBeing(res)))
+    .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
 );
 
 export const updateBeing = (being) => dispatch => (
-    BeingsUtil.updateBeing(being)
-        .then(res => dispatch(receiveBeing(res)))
-        .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
+  BeingsUtil.updateBeing(being)
+    .then(res => dispatch(receiveBeing(res)))
+    .fail(jqXHR => dispatch(receiveBeingsErrors(jqXHR.responseJSON)))
 );
