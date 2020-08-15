e399126ca5368b31cb955bc0b038078e60c81a11
M3L6H <sisoac@hotmail.com>
2020-08-14 23:56:32 -0700

    Add routes for comrades

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
-    namespace :api do
-        resources :beings, except: [:new, :edit]
-        resources :clusters, only: [:create, :destroy, :update]
-
-        resources :dimensions, except: [:new, :edit] do
-            resources :dimension_beings, only: [:create]
-            resources :roles, only: [:create]
-        end
-
-        resources :dimension_beings, only: [:show, :destroy, :update]
-        resources :missives, only: [:create, :update]
-        resources :realms, except: [:index, :new, :edit]
-        resources :roles, only: [:show, :destroy, :update]
-        resources :summonses, only: [:create, :destroy, :update]
-        resource :session, only: [:create, :destroy]
-
-        patch "/dimensions/:id/update_order", to: "dimensions#update_order"
-        put "/dimensions/:id/update_order", to: "dimensions#update_order"
+  namespace :api do
+    resources :beings, except: [:new, :edit]
+    resources :clusters, only: [:create, :destroy, :update]
+    resources :comrades, only: [:create, :delete, :update]
+    
+    resources :dimensions, except: [:new, :edit] do
+        resources :dimension_beings, only: [:create]
+        resources :roles, only: [:create]
     end
 
-    mount ActionCable.server => "/cable"
+    resources :dimension_beings, only: [:show, :destroy, :update]
+    resources :missives, only: [:create, :update]
+    resources :realms, except: [:index, :new, :edit]
+    resources :roles, only: [:show, :destroy, :update]
+    resources :summonses, only: [:create, :destroy, :update]
+    resource :session, only: [:create, :destroy]
 
-    root to: "static_pages#root"
+    patch "/dimensions/:id/update_order", to: "dimensions#update_order"
+    put "/dimensions/:id/update_order", to: "dimensions#update_order"
+  end
 
-    get "/join/:url", to: redirect("/#/join?url=%{url}")
-    
-    get "*path" => redirect("/")
+  mount ActionCable.server => "/cable"
+
+  root to: "static_pages#root"
+
+  get "/join/:url", to: redirect("/#/join?url=%{url}")
+  
+  get "*path" => redirect("/")
 end
