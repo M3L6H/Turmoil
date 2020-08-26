29ad368d9c15dbf0d26f6bc564b41efc50d90759
M3L6H <sisoac@hotmail.com>
2020-08-25 23:44:40 -0700

    Add friends modal reducer

--- frontend/reducers/modals_reducer.js
+++ frontend/reducers/modals_reducer.js
@@ -1,56 +1,62 @@
 import { 
-    RECEIVE_AUTH_MODAL,
-    RECEIVE_CLUSTER_MODAL,
-    RECEIVE_DIMENSION_MODAL,
-    RECEIVE_EDIT_REALM_MODAL,
-    RECEIVE_REALM_MODAL,
-    RECEIVE_SUMMONS_MODAL
+  RECEIVE_AUTH_MODAL,
+  RECEIVE_CLUSTER_MODAL,
+  RECEIVE_DIMENSION_MODAL,
+  RECEIVE_FRIENDS_MODAL,
+  RECEIVE_EDIT_REALM_MODAL,
+  RECEIVE_REALM_MODAL,
+  RECEIVE_SUMMONS_MODAL,
 } from '../actions/modals_actions';
 
 import { REMOVE_SESSION } from '../actions/session_actions';
 
 const _defaultState = {
-    auth: {
-        open: false,
-        formType: "signIn"
-    },
-    clusters: {
-        open: false
-    },
-    dimensions: {
-        open: false
-    },
-    editRealms: {
-        open: false
-    },
-    realms: {
-        open: false
-    },
-    summonses: {
-        open: false,
-        formType: "new"
-    }
+  auth: {
+    open: false,
+    formType: "signIn"
+  },
+  clusters: {
+    open: false
+  },
+  dimensions: {
+    open: false
+  },
+  editRealms: {
+    open: false
+  },
+  friends: {
+    open: false
+  },
+  realms: {
+    open: false
+  },
+  summonses: {
+    open: false,
+    formType: "new"
+  }
 };
 
 export default (state=_defaultState, action) => {
-    Object.freeze(state);
+  Object.freeze(state);
 
-    switch (action.type) {
-        case RECEIVE_AUTH_MODAL:
-            return { ...state, auth: action.modal };
-        case RECEIVE_CLUSTER_MODAL:
-            return { ...state, clusters: action.modal };
-        case RECEIVE_DIMENSION_MODAL:
-            return { ...state, dimensions: action.modal };
-        case RECEIVE_EDIT_REALM_MODAL:
-            return { ...state, editRealms: action.modal };
-        case RECEIVE_REALM_MODAL:
-            return { ...state, realms: action.modal };
-        case RECEIVE_SUMMONS_MODAL:
-            return { ...state, summonses: action.modal };
-        case REMOVE_SESSION:
-            return _defaultState;
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case RECEIVE_AUTH_MODAL:
+      return { ...state, auth: action.modal };
+    case RECEIVE_CLUSTER_MODAL:
+      return { ...state, clusters: action.modal };
+    case RECEIVE_DIMENSION_MODAL:
+      return { ...state, dimensions: action.modal };
+    case RECEIVE_EDIT_REALM_MODAL:
+      return { ...state, editRealms: action.modal };
+    case RECEIVE_FRIENDS_MODAL:
+      return { ...state, friends: action.modal };
+    case RECEIVE_REALM_MODAL:
+      return { ...state, realms: action.modal };
+    case RECEIVE_SUMMONS_MODAL:
+      return { ...state, summonses: action.modal };
+    case REMOVE_SESSION:
+      return _defaultState;
+    default:
+      return state;
+  }
 };
