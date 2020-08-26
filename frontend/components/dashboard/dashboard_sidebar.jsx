306e1fb9fd84c2bffbed1783c45d556712d9c5d4
M3L6H <sisoac@hotmail.com>
2020-08-25 23:04:30 -0700

    Add + button for friends list

--- frontend/components/dashboard/dashboard_sidebar.jsx
+++ frontend/components/dashboard/dashboard_sidebar.jsx
@@ -5,8 +5,11 @@ import { Header, Icon, Menu } from '../shoebuckle';
 export default (props) => {
   return (
     <>
-      <Menu.Item justifyStart>
-        <Icon name="user-friends" className="friends-icon" /> Friends
+      <Menu.Item>
+        <span>
+          <Icon name="user-friends" className="friends-icon" /> Friends
+        </span>
+        <Icon name="plus" />
       </Menu.Item>
       <Menu.Item>
         <Header as="h4">Direct Messages</Header>
