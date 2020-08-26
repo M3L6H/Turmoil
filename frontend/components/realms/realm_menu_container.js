99846eb5821c4266710729762f2b245546a30f57
M3L6H <sisoac@hotmail.com>
2020-08-25 23:05:13 -0700

    Fix indentation

--- frontend/components/realms/realm_menu_container.js
+++ frontend/components/realms/realm_menu_container.js
@@ -5,10 +5,10 @@ import RealmMenu from './realm_menu';
 import { deleteCluster } from '../../actions/clusters_actions';
 import { receiveContext } from '../../actions/context_menu_actions';
 import { 
-    receiveClusterModal, 
-    receiveRealmModal, 
-    receiveSummonsModal, 
-    receiveEditRealmModal
+  receiveClusterModal, 
+  receiveRealmModal, 
+  receiveSummonsModal, 
+  receiveEditRealmModal
 } from '../../actions/modals_actions';
 import { deleteDimension, updateOrder } from '../../actions/dimensions_actions';
 import { selectDimension } from '../../actions/dimension_menu_actions';
@@ -17,25 +17,25 @@ import { selectRealm } from '../../actions/realm_menu_actions';
 import { receiveSidebar } from '../../actions/sidebar_actions';
 
 const mapStateToProps = (state, ownProps) => ({
-    ...ownProps,
-    dimension: state.entities.dimensions[state.ui.menus.dimensionMenu.selected],
-    clusters: state.entities.clusters,
-    realms: state.entities.realms
+  ...ownProps,
+  dimension: state.entities.dimensions[state.ui.menus.dimensionMenu.selected],
+  clusters: state.entities.clusters,
+  realms: state.entities.realms
 });
 
 const mapDispatchToProps = (dispatch) => ({
-    closeSidebar: () => dispatch(receiveSidebar({ open: false })),
-    select: selected => dispatch(selectRealm(selected)),
-    selectDimension: selected => dispatch(selectDimension(selected)),
-    updateOrder: (dimensionId, data) => dispatch(updateOrder(dimensionId, data)),
-    openClusterForm: () => dispatch(receiveClusterModal(true)),
-    openRealmForm: () => dispatch(receiveRealmModal(true)),
-    openEditRealmForm: () => dispatch(receiveEditRealmModal(true)),
-    openSummonsForm: () => dispatch(receiveSummonsModal(true)),
-    setUpContextMenu: (items, name) => dispatch(receiveContext(items, name)),
-    deleteCluster: (id) => dispatch(deleteCluster(id)),
-    deleteRealm: (id) => dispatch(deleteRealm(id)),
-    deleteDimension: (id) => dispatch(deleteDimension(id))
+  closeSidebar: () => dispatch(receiveSidebar({ open: false })),
+  select: selected => dispatch(selectRealm(selected)),
+  selectDimension: selected => dispatch(selectDimension(selected)),
+  updateOrder: (dimensionId, data) => dispatch(updateOrder(dimensionId, data)),
+  openClusterForm: () => dispatch(receiveClusterModal(true)),
+  openRealmForm: () => dispatch(receiveRealmModal(true)),
+  openEditRealmForm: () => dispatch(receiveEditRealmModal(true)),
+  openSummonsForm: () => dispatch(receiveSummonsModal(true)),
+  setUpContextMenu: (items, name) => dispatch(receiveContext(items, name)),
+  deleteCluster: (id) => dispatch(deleteCluster(id)),
+  deleteRealm: (id) => dispatch(deleteRealm(id)),
+  deleteDimension: (id) => dispatch(deleteDimension(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RealmMenu);
