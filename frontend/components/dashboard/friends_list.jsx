bcffc0a4e033f8409b780c5d2d66e90d6ee2b17f
M3L6H <sisoac@hotmail.com>
2020-08-25 23:24:54 -0700

    Improving menu item styling

--- frontend/components/dashboard/friends_list.jsx
+++ frontend/components/dashboard/friends_list.jsx
@@ -5,7 +5,10 @@ import { Menu } from '../shoebuckle';
 export default ({ comrades }) => {
   if (comrades.length === 0) {
     return (
-      <Menu.Menu>
+      <Menu.Menu 
+        noHover
+        vertical
+      >
         <Menu.Item>
           You currently have no friends. Use the + button to search for people you know!
         </Menu.Item>
