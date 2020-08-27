2a15873fa466da06fb1d3b4d7968bb39af917984
M3L6H <sisoac@hotmail.com>
2020-08-26 23:35:41 -0700

    Use search beings

--- frontend/components/dashboard/dashboard_sidebar_container.js
+++ frontend/components/dashboard/dashboard_sidebar_container.js
@@ -1,18 +1,18 @@
 import { connect } from 'react-redux';
 
-import { fetchBeings } from '../../actions/beings_actions';
+import { searchBeings } from '../../actions/beings_actions';
 import { receiveFriendsModal } from '../../actions/modals_actions';
 
 import DashboardSidebar from './dashboard_sidebar';
 
 const mapStateToProps = (state) => ({
   friendsModal: state.ui.modals.friends,
-  beings: state.entities.beings
+  beings: state.entities.searchBeings
 });
 
 const mapDispatchToProps = (dispatch) => ({
   receiveFriendsModal: (open) => dispatch(receiveFriendsModal(open)),
-  fetchBeings: (search) => dispatch(fetchBeings(search))
+  fetchBeings: (search) => dispatch(searchBeings(search))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardSidebar);
