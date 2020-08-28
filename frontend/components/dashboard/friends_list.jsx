e3757cb814f74371cac7f7721d33efebcfe56c29
M3L6H <sisoac@hotmail.com>
2020-08-27 23:41:43 -0700

    Fix friends list counting un-added friends as friends

--- frontend/components/dashboard/friends_list.jsx
+++ frontend/components/dashboard/friends_list.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Menu } from '../shoebuckle';
 
 export default ({ comrades, inverted }) => {
+  comrades = comrades.filter(({ pending, blocked }) => !(pending || blocked ));
 
   return (
     <Menu.Menu
