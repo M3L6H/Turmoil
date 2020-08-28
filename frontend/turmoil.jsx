63151058beb7fc100634f694c375b1d5a1455892
M3L6H <sisoac@hotmail.com>
2020-08-27 23:38:47 -0700

    Fix preloaded state

--- frontend/turmoil.jsx
+++ frontend/turmoil.jsx
@@ -8,35 +8,38 @@ import configureStore from './store/store';
 import { receiveCable } from './actions/cable_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-    let store;
+  let store;
 
-    if (window.currentDetails) { 
-        const { being, dimensions } = window.currentDetails;
-        
-        const preloadedState = {
-            entities: {
-                beings: { [being.id]: being },
-                dimensions
-            },
-            session: { currentBeingId: being.id }
-        };
-        
-        store = configureStore(preloadedState);
-        delete window.currentDetails; 
-    } else {
-        store = configureStore();
-    }
+  if (window.currentDetails) { 
+    console.log(window.currentDetails);
+    const { being, dimensions, comrades, comradeBeings } = window.currentDetails;
+    
+    const preloadedState = {
+      entities: {
+        beings: { [being.id]: being },
+        dimensions,
+        comrades: comrades || {},
+        comradeBeings: comradeBeings || {}
+      },
+      session: { currentBeingId: being.id }
+    };
+    
+    store = configureStore(preloadedState);
+    delete window.currentDetails; 
+  } else {
+    store = configureStore();
+  }
 
-    // Set up action cable
-    const cable = actionCable.createConsumer(`ws://${ window.location.host }/cable`);
-    store.dispatch(receiveCable(cable));
+  // Set up action cable
+  const cable = actionCable.createConsumer(`ws://${ window.location.host }/cable`);
+  store.dispatch(receiveCable(cable));
 
-    // Set up viewport height and width for mobile
-    const vh = window.innerHeight * 0.01;
-    const vw = window.innerWidth * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-    document.documentElement.style.setProperty("--vw", `${vw}px`);
+  // Set up viewport height and width for mobile
+  const vh = window.innerHeight * 0.01;
+  const vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  document.documentElement.style.setProperty("--vw", `${vw}px`);
 
-    const root = document.getElementById("root");
-    ReactDOM.render(<Root store={ store } />, root);
+  const root = document.getElementById("root");
+  ReactDOM.render(<Root store={ store } />, root);
 });
