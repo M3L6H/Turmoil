b80bb26ed4c211f402aed7d3bc7822d4919dd0fb
M3L6H <sisoac@hotmail.com>
2020-08-29 23:28:34 -0700

    Add dashboard actions

---
+++ frontend/actions/dashboard_actions.js
@@ -0,0 +1,6 @@
+export const RECEIVE_DASHBOARD_SELECTION = "RECEIVE_DASHBOARD_SELECTION";
+
+export const receiveDashboardSelection = selection => ({
+  type: RECEIVE_DASHBOARD_SELECTION,
+  selection
+});
