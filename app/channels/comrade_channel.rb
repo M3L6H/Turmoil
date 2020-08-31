85a7f05809ffbc52c5f5c3bbaaf021f0f90e2297
M3L6H <sisoac@hotmail.com>
2020-08-31 11:53:53 -0700

    Add comrade channel

---
+++ app/channels/comrade_channel.rb
@@ -0,0 +1,11 @@
+class ComradeChannel < ApplicationCable::Channel
+  def subscribed
+    being_id = params[:being_id]
+    @comrades = Comrades.where("being_id = ? OR comrade_id = ?", being_id, being_id)
+    stream_for @comrades
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
