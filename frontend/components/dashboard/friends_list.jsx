5ba208d5c3de3222a0f060fafb0ec8bf4bea502a
M3L6H <sisoac@hotmail.com>
2020-08-30 23:24:25 -0700

    Add invite button to friends list

--- frontend/components/dashboard/friends_list.jsx
+++ frontend/components/dashboard/friends_list.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-import { Header, Section } from '../shoebuckle';
+import { Header, Icon, Section } from '../shoebuckle';
 
-export default ({ comrades, inverted }) => {
+export default ({ comrades, inverted, tab }) => {
   comrades = comrades.filter(({ pending, blocked }) => !(pending || blocked ));
+
+  let header;
+
+  switch (tab) {
+    default:
+      header = (
+        <div className="friends-list-header-container">
+          <Header as="h3" inline inverted={ inverted }>All Friends</Header>
+          <Icon large name="plus" />
+        </div>
+      );
+  }
 
   return (
     <Section inverted={ inverted } className="friends-list-section">
-      <Header as="h3" inverted={ inverted }>All Friends</Header>
+      { header }
     </Section>
   );
 };
--- frontend/components/dashboard/friends_list_container.js
+++ frontend/components/dashboard/friends_list_container.js
@@ -5,7 +5,8 @@ import { selectComrades } from '../../reducers/selectors';
 import FriendsList from './friends_list';
 
 const mapStateToProps = (state) => ({
-  comrades: selectComrades(state)
+  comrades: selectComrades(state),
+  tab: state.ui.dashboard.tab
 });
 
 export default connect(mapStateToProps, null)(FriendsList);
