525233bb25a8132cf21bbe1fee6d4be9c06c184f
M3L6H <sisoac@hotmail.com>
2020-09-02 00:28:32 -0700

    Add create conversation action

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -2,4 +2,19 @@ class Api::ConversationsController < ApplicationController
   def show
     @conversation = Conversation.find_by(id: params[:id])
   end
+
+  def create
+    @conversation = Conversation.new(conversation_params)
+
+    if @conversation.save
+      render :create
+    else
+      render json: @conversation.errors.full_messages, status: 422
+    end
+  end
+
+private
+  def conversation_params
+    params.require(:conversation).permit(:being_id, :type)
+  end
 end
