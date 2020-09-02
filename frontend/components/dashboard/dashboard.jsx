4e57b70fe12c65e385843d8cd3e22b2c86ef580c
M3L6H <sisoac@hotmail.com>
2020-09-02 04:55:46 -0700

    Render chat window when conversation is selected

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import ChatWindow from '../chat_window';
 import FriendsList from './friends_list_container';
 
 export default ({ dashboard, ...otherProps }) => {
   switch (dashboard.selected) {
+    case "conversation":
+      return <ChatWindow { ...otherProps } />
     default:
       return <FriendsList { ...otherProps } />
   }
