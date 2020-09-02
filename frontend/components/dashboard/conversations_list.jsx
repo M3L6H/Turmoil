5788cd941ea429c2fb9b6818e0bc0079a7933f7f
M3L6H <sisoac@hotmail.com>
2020-09-02 04:44:58 -0700

    Fix up conversation selections

--- frontend/components/dashboard/conversations_list.jsx
+++ frontend/components/dashboard/conversations_list.jsx
@@ -4,12 +4,24 @@ import { Menu } from '../shoebuckle';
 
 import truncate from '../../util/truncate';
 
-export default ({ beings, conversations, beingConversations }) => {
+export default ({
+  beings,
+  conversations,
+  beingConversations,
+  receiveDashboardSelection,
+  receiveHeader
+}) => {
 
   return (
     <Menu.Menu fluid vertical inverted className="conversation-list">
       { conversations.map(conv => (
-        <Menu.Item key={ conv.id }>
+        <Menu.Item
+          key={ conv.id }
+          onClick={ () => {
+            receiveDashboardSelection({ selected: "conversation", conversation: conv.id });
+            receiveHeader("conversation");
+          } }
+        >
           { truncate(beingConversations.filter(({ conversationId }) => conversationId === conv.id).map(({ beingId }) => beings[beingId].username).concat([beings[conv.beingId].username]).join(", ")) }
         </Menu.Item>
       )) }
--- frontend/components/dashboard/conversations_list_container.js
+++ frontend/components/dashboard/conversations_list_container.js
@@ -2,10 +2,18 @@ import { connect } from 'react-redux';
 
 import ConversationsList from './conversations_list';
 
+import { receiveDashboardSelection } from '../../actions/dashboard_actions';
+import { receiveHeader } from '../../actions/header_actions';
+
 const mapStateToProps = ({ entities: { beings, conversations, beingConversations } }) => ({
   beings,
   conversations: conversations ? Object.values(conversations) : [],
   beingConversations: beingConversations ? Object.values(beingConversations) : []
 });
 
-export default connect(mapStateToProps, null)(ConversationsList);
+const mapDispatchToProps = (dispatch) => ({
+  receiveDashboardSelection: (dashboard) => dispatch(receiveDashboardSelection(dashboard)),
+  receiveHeader: (header) => dispatch(receiveHeader(header))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConversationsList);
