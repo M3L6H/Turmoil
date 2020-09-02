c3ff632a75aef540cfe3c274eec2723a345ac2c4
M3L6H <sisoac@hotmail.com>
2020-09-02 11:08:30 -0700

    Hide voice realms

--- frontend/components/realms/realm_form.jsx
+++ frontend/components/realms/realm_form.jsx
@@ -3,98 +3,100 @@ import React, { Component } from 'react';
 import { Button, Form, Icon, Modal } from '../shoebuckle';
 
 export default class RealmForm extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            name: "",
-            realmType: "text",
-            errors: []
-        };
+    this.state = {
+      name: "",
+      realmType: "text",
+      errors: []
+    };
 
-        this._handleClose = this._handleClose.bind(this);
-        this._handleChange = this._handleChange.bind(this);
-        this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleClose = this._handleClose.bind(this);
+    this._handleChange = this._handleChange.bind(this);
+    this._handleSubmit = this._handleSubmit.bind(this);
+  }
+  
+  _handleClose() {
+    this.props.closeForm();
+    this.setState({ name: "", realmType: "text", errors: [] });
+  }
+  
+  _handleChange(e) {
+    const input = e.currentTarget;
+
+    switch (input.dataset.type) {
+      case "name":
+        const sanitized = input.value.toLowerCase().replace(" ", "-").replace(/[^\w\-]/, "");
+        this.setState({ [input.dataset.type]: sanitized });
+        break;
+      case "realmType":
+        this.setState({ [input.dataset.type]: input.value || input.dataset.value });
     }
+  }
+
+  _handleSubmit(e) {
+    e.preventDefault();
     
-    _handleClose() {
-        this.props.closeForm();
-        this.setState({ name: "", realmType: "text", errors: [] });
-    }
-    
-    _handleChange(e) {
-        const input = e.currentTarget;
+    const { dimensionId, firstOrderable, createRealm } = this.props;
+    const { name, realmType } = this.state;
+    const realm = {
+      name,
+      dimension_id: dimensionId,
+      realm_type: realmType,
+      next_orderable_id: firstOrderable.id,
+      next_orderable_type: firstOrderable.type
+    };
 
-        switch (input.dataset.type) {
-            case "name":
-                const sanitized = input.value.toLowerCase().replace(" ", "-").replace(/[^\w\-]/, "");
-                this.setState({ [input.dataset.type]: sanitized });
-                break;
-            case "realmType":
-                this.setState({ [input.dataset.type]: input.value || input.dataset.value });
-        }
-    }
+    createRealm(realm)
+      .then(this._handleClose)
+      .fail(jqXHR => this.setState({ errors: jqXHR.responseJSON }));
+  }
 
-    _handleSubmit(e) {
-        e.preventDefault();
-        
-        const { dimensionId, firstOrderable, createRealm } = this.props;
-        const { name, realmType } = this.state;
-        const realm = {
-            name,
-            dimension_id: dimensionId,
-            realm_type: realmType,
-            next_orderable_id: firstOrderable.id,
-            next_orderable_type: firstOrderable.type
-        };
+  _renderForm() {
+    const { name, realmType, errors } = this.state;
 
-        createRealm(realm)
-            .then(this._handleClose)
-            .fail(jqXHR => this.setState({ errors: jqXHR.responseJSON }));
-    }
+    // ,
+    //                 { value: "voice", label: <><Icon name="microphone" />Voice Realm</> }
 
-    _renderForm() {
-        const { name, realmType, errors } = this.state;
+    return (
+      <Form onSubmit={ this._handleSubmit } errors={ errors }>
+        <Form.RadioGroup 
+          label="Realm Type"
+          data-type="realmType"
+          onChange={ this._handleChange }
+          selected={ realmType }
+          options={[
+              { value: "text", label: <><Icon name="hashtag" />Text Realm</> }
+          ]}
+        />
+        <Form.Input 
+          label="Realm Name" 
+          data-type="name"
+          onChange={ this._handleChange }
+          value={ name }
+        />
+        <Button animated fluid green type="submit">
+          <Button.Content visible>Create Realm</Button.Content>
+          <Button.Content hidden>
+            <Icon name="arrow-right" />
+          </Button.Content>
+        </Button>
+      </Form>
+    );
+  }
+  
+  render() {
+    const { realmType } = this.state;
+    const { open, inverted } = this.props;
 
-        return (
-            <Form onSubmit={ this._handleSubmit } errors={ errors }>
-                <Form.RadioGroup 
-                    label="Realm Type"
-                    data-type="realmType"
-                    onChange={ this._handleChange }
-                    selected={ realmType }
-                    options={[
-                        { value: "text", label: <><Icon name="hashtag" />Text Realm</> },
-                        { value: "voice", label: <><Icon name="microphone" />Voice Realm</> }
-                    ]}
-                />
-                <Form.Input 
-                    label="Realm Name" 
-                    data-type="name"
-                    onChange={ this._handleChange }
-                    value={ name }
-                />
-                <Button animated fluid green type="submit">
-                    <Button.Content visible>Create Realm</Button.Content>
-                    <Button.Content hidden>
-                        <Icon name="arrow-right" />
-                    </Button.Content>
-                </Button>
-            </Form>
-        );
-    }
-    
-    render() {
-        const { realmType } = this.state;
-        const { open, inverted } = this.props;
-
-        return (
-            <Modal handleClose={ this._handleClose } open={ open } inverted={ inverted }>
-                <Modal.Header>{`Create ${ realmType === "text" ? "Text" : "Voice" } Realm`}</Modal.Header>
-                <Modal.Content>
-                    { this._renderForm() }
-                </Modal.Content>
-            </Modal>
-        );
-    }
+    return (
+      <Modal handleClose={ this._handleClose } open={ open } inverted={ inverted }>
+        <Modal.Header>{`Create ${ realmType === "text" ? "Text" : "Voice" } Realm`}</Modal.Header>
+        <Modal.Content>
+          { this._renderForm() }
+        </Modal.Content>
+    </Modal>
+    );
+  }
 }
