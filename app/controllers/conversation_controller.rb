38df9c516794a255ee6d4cc3cb9d175b7d8367c5
M3L6H <sisoac@hotmail.com>
2020-09-02 00:06:44 -0700

    Add conversation controller

---
+++ app/controllers/conversation_controller.rb
@@ -0,0 +1,2 @@
+class ConversationController < ApplicationController
+end
---
+++ test/controllers/conversation_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConversationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
