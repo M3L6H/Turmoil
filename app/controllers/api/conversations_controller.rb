c3e468d5abbbe17fd2a22bc90b2c2d930a582dc5
M3L6H <sisoac@hotmail.com>
2020-09-02 00:32:48 -0700

    Add delete covnersation action

--- app/controllers/api/conversations_controller.rb
+++ app/controllers/api/conversations_controller.rb
@@ -1,4 +1,7 @@
 class Api::ConversationsController < ApplicationController
+  before_action :require_json
+  before_action :require_logged_in
+
   def show
     @conversation = Conversation.find_by(id: params[:id])
   end
@@ -10,6 +13,23 @@ class Api::ConversationsController < ApplicationController
       render :create
     else
       render json: @conversation.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @conversation = Conversation.find_by(id: params[:id])
+
+    if @conversation
+
+      if current_being.id != @conversation.being_id 
+        render json: ["Cannot delete a conversation you didn't create!"], status: 403
+        return
+      end
+
+      @conversation.destroy
+      render :destroy
+    else
+      render json: ["Could not find conversation with id #{ params[:id] }"], status: 404
     end
   end
 
